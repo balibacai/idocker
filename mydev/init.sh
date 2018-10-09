6f7c30fcdb1a22b5bc2ca278cd4e60ee5f387431
mistcheng <mistcheng@tencent.com>
2018-10-09 20:35:38 +0800

    modify init.sh

--- mydev/init.sh
+++ mydev/init.sh
@@ -3,8 +3,8 @@
 # pre build
 docker build -t sshd_centos:v1.1 sshd_centos
 docker build -t go_centos:v1.1 go_centos
-docker build -t beego_centos:v1.1 beego_centos
-docker build -t node_centos:v1.1 node_centos
+# docker build -t beego_centos:v1.1 beego_centos
+# docker build -t node_centos:v1.1 node_centos
 
 # up services
 docker-compose up -d
